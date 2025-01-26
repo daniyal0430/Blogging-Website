6b9cce8d2e9e56f5701b6710269860ce88d71e22
l1f21bsse0430 <l1f21bsse0430@ucp.edu.pk>
2025-01-26 15:28:42 +0500

    Initial Commit

---
+++ config.sh
@@ -0,0 +1,6 @@
+echo "Going to Config the Repo"
+git branch -m develop
+git config --local user.name "l1f21bsse0430"
+git config --local user.email "l1f21bsse0430@ucp.edu.pk"
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css .gitignore
